1a78f8328ba869eebb8b89d005e2d8a926b67e74
Edlaine Zamora <edlainezamora@ip-192-168-0-12.ap-southeast-1.compute.internal>
2020-04-26 15:25:19 -0300

    Add react-router-dom dependency and create routes

---
+++ src/components/PageNotFound/PageNotFound.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+const PageNotFound = () => {
+    return (
+        <Fragment>
+            <h1>Page not Found</h1>
+        </Fragment>
+    );
+}
+
+export default PageNotFound;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route} from 'react-router-dom';
 import App from './App';
+import PageNotFound from './components/PageNotFound/PageNotFound';
+
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact={true} component={App}/>
+                <Route path="/conferences" component={App}/>
+                <Route component={PageNotFound}/>
+            </Switch>
+        </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
 );
